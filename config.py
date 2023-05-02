3962021b216ed6e28ae26cd9f80e4a8005952eb5
vikioza <vikioza@gmail.com>
2023-05-02 14:47:50 +0200

    random fixes to enable training

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ CONFIG = {
         "lr": 0.0001,
     },
     "dataset": {
-        "img_dir": "data/images/",
-        "annotations_file": "data/labels.csv",
+        "img_dir": "",
+        "annotations_file": "data/labels_new.csv",
         "img_w": 640,
         "img_h": 640,
         "num_anchors_per_cell": 3,
     },
     "dataloader": {
         "batch_size": 32,
-    }
+    },
 }
--- dataloader_utils.py
+++ dataloader_utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
-from torchvision.io import read_image
+from torchvision.io import read_image, ImageReadMode
 from torchvision.ops import box_iou
 import torchvision.transforms.functional as fn
 import torchvision.transforms as transforms
@@ -11,8 +11,22 @@ import math
 
 
 class MTGCardsDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, anchor_boxes, feature_map_dims, img_dims, num_anchors_per_cell, num_max_boxes, transform=None, target_transform=None):
+    def __init__(
+        self,
+        annotations_file,
+        img_dir,
+        anchor_boxes,
+        feature_map_dims,
+        img_dims,
+        num_anchors_per_cell,
+        num_max_boxes,
+        transform=None,
+        target_transform=None,
+        limit=None,
+    ):
         self.img_labels = pd.read_csv(annotations_file)
+        self.limit = len(self.img_labels) if limit is None else limit
+
         self.img_dir = img_dir
 
         self.anchor_boxes = anchor_boxes
@@ -29,14 +43,15 @@ class MTGCardsDataset(Dataset):
         self.target_transform = target_transform
 
         self.target_labels = self.generate_target_labels()
-    
+
     def generate_target_labels(self):
         target_labels = []
-        for idx in range(len(self.img_labels)):
-            label = np.array(self.img_labels.iloc[idx, [1,2,3,4]].values, dtype=float)
+        for idx in range(self.limit):
+            label = np.array(
+                self.img_labels.iloc[idx, [1, 2, 3, 4]].values, dtype=float
+            )
             target_labels.append(self.generate_feature_label(label=label))
         return target_labels
-
 
     def get_ground_truth(self, label):
         real_cx = label[-4]
@@ -44,15 +59,23 @@ class MTGCardsDataset(Dataset):
         real_w = label[-2]
         real_h = label[-1]
 
-        label_cell_x = int(real_cx / self.scale_w)  # the cell in whice the box is centered
+        label_cell_x = int(
+            real_cx / self.scale_w
+        )  # the cell in whice the box is centered
         label_cell_y = int(real_cy / self.scale_h)
-        label_cx = (real_cx / self.scale_w) - label_cell_x  # offset from the 0,0 of the cell
+        label_cx = (
+            real_cx / self.scale_w
+        ) - label_cell_x  # offset from the 0,0 of the cell
         label_cy = (real_cy / self.scale_h) - label_cell_y
         label_w = real_w / self.scale_w  # width and height of the box in cells
         label_h = real_h / self.scale_h
-        return torch.Tensor([label_cell_x, label_cell_y, label_cx, label_cy, label_w, label_h])
-    
-    def get_ground_truth_real_coords(self, label):  # convets the box from (cx, cy, w, h) to (x1, y1, x2, y2) - TODO: maybe we can do this using torchvision.ops.box_convert  
+        return torch.Tensor(
+            [label_cell_x, label_cell_y, label_cx, label_cy, label_w, label_h]
+        )
+
+    def get_ground_truth_real_coords(
+        self, label
+    ):  # convets the box from (cx, cy, w, h) to (x1, y1, x2, y2) - TODO: maybe we can do this using torchvision.ops.box_convert
         real_cx = label[-4]
         real_cy = label[-3]
         real_w = label[-2]
@@ -62,7 +85,7 @@ class MTGCardsDataset(Dataset):
         real_x2 = real_cx + (real_w / 2)
         real_y2 = real_cy + (real_h / 2)
         return torch.Tensor([real_x1, real_y1, real_x2, real_y2])
-    
+
     def get_anchor(self, anchor, pos_x, pos_y):
         anchor_cx = pos_x * self.scale_w
         anchor_cy = pos_y * self.scale_h
@@ -72,18 +95,20 @@ class MTGCardsDataset(Dataset):
         anchor_y2 = anchor_cy + (anchor[1] / 2)
         return torch.Tensor([anchor_x1, anchor_y1, anchor_x2, anchor_y2])
 
-    def generate_feature_label(self, label): 
+    def generate_feature_label(self, label):
         gt_object = self.get_ground_truth(label=label)
         gt_real_coords = self.get_ground_truth_real_coords(label=label)
-        target = torch.zeros((self.feature_map_w, self.feature_map_h, self.num_anchors_per_cell, 5))    # create an empty ground truth
+        target = torch.zeros(
+            (self.feature_map_w, self.feature_map_h, self.num_anchors_per_cell, 5)
+        )  # create an empty ground truth
 
-        #iou_matrix = torch.zeros((self.feature_map_w, self.feature_map_h, self.num_anchors_per_cell))
-        #max_iou = 0
-        #max_iou_coords = [0, 0, 0]
-        #for i in range(self.feature_map_w):
+        # iou_matrix = torch.zeros((self.feature_map_w, self.feature_map_h, self.num_anchors_per_cell))
+        # max_iou = 0
+        # max_iou_coords = [0, 0, 0]
+        # for i in range(self.feature_map_w):
         #    for j in range(self.feature_map_h):
         #        for k in range(self.num_anchors_per_cell):
-        #            anchor = self.get_anchor(self.anchor_boxes[k], (i + gt_object[0]), (j + gt_object[1])) # box shifted by the x, y center offset of ground truth 
+        #            anchor = self.get_anchor(self.anchor_boxes[k], (i + gt_object[0]), (j + gt_object[1])) # box shifted by the x, y center offset of ground truth
         #            iou = box_iou(gt_real_coords.unsqueeze(0), anchor.unsqueeze(0))
         #            if iou > max_iou:
         #                max_iou = iou
@@ -92,45 +117,51 @@ class MTGCardsDataset(Dataset):
 
         cell_x_idx = int(gt_object[0])
         cell_y_idx = int(gt_object[1])
-        
+
         max_iou = 0
         anchor_idx = 0
         for i in range(len(self.anchor_boxes)):
-            anchor = self.get_anchor(self.anchor_boxes[i], (gt_object[0] + gt_object[2]), (gt_object[1] + gt_object[3])) # box shifted by the x, y center offset of ground truth 
+            anchor = self.get_anchor(
+                self.anchor_boxes[i],
+                (gt_object[0] + gt_object[2]),
+                (gt_object[1] + gt_object[3]),
+            )  # box shifted by the x, y center offset of ground truth
             iou = box_iou(gt_real_coords.unsqueeze(0), anchor.unsqueeze(0))
             if iou > max_iou:
                 max_iou = iou
                 anchor_idx = i
 
-        target[cell_x_idx, cell_y_idx, anchor_idx, 0] = 1.0 
+        target[cell_x_idx, cell_y_idx, anchor_idx, 0] = 1.0
         tx = gt_object[2]
         ty = gt_object[3]
         tw = label[-2] / self.anchor_boxes[anchor_idx][0]
         th = label[-1] / self.anchor_boxes[anchor_idx][1]
         target[cell_x_idx, cell_y_idx, anchor_idx, 1:5] = torch.Tensor([tx, ty, tw, th])
-        #print(f"RETURNED TARGET: {target[cell_x_idx, cell_y_idx, anchor_idx, :]}")
+        # print(f"RETURNED TARGET: {target[cell_x_idx, cell_y_idx, anchor_idx, :]}")
         return target
 
     def __len__(self):
-        return len(self.img_labels)
+        return self.limit
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        #label = np.array(self.img_labels.iloc[idx, [1,2,3,4]].values, dtype=float)
+        image = read_image(img_path, mode=ImageReadMode.RGB)
+        # label = np.array(self.img_labels.iloc[idx, [1,2,3,4]].values, dtype=float)
         label = self.target_labels[idx]
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
         return image, label
-    
+
 
 def get_transform_pipe(img_w, img_h):
-    transform_pipe = transforms.Compose([
-        transforms.Resize([img_w,img_h]),
-        transforms.ToPILImage(),
-        transforms.ToTensor(),
-        transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]),
-    ])
+    transform_pipe = transforms.Compose(
+        [
+            transforms.Resize([img_w, img_h]),
+            transforms.ToPILImage(mode="RGB"),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
     return transform_pipe
